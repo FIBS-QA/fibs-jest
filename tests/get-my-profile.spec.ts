19d702f3866cf915f76e956f43523afb63efa038
Csák <attila.csak@hu.fressnapf.eu>
2023-01-18 11:23:18 +0100

    Add ts-node add baseUrl

--- tests/get-my-profile.spec.ts
+++ tests/get-my-profile.spec.ts
@@ -1,13 +1,13 @@
 
 const express = require('express');
-const request = require('supertest');
+const request = require('supertest')('https://webshop.fressnapf.hu');
 
 const app = express();
 
 describe('Fressnapf webshop', () => {
   it('test my profile endpoint', async() => {      
       try {
-        const res = await request(app).get('https://webshop.fressnapf.hu/profilom');
+        const res = await request(app).get('/profilom');
         expect(res).toBe(200);  
       } catch (err) {
         console.log(`${err}`);
--- tests/search.spec.ts
+++ tests/search.spec.ts
@@ -1,6 +1,6 @@
 
 const exp = require('express');
-const req = require('supertest');
+const req = require('supertest')('https://webshop.fressnapf.hu');
 
 const appSearch = exp();
 
@@ -8,7 +8,7 @@ describe('Search in Fressnapf webshop', () => {
     it('test search endpoint', async() => {
       try {
         const searchItem= 'nyakörv';
-        const res = await req(appSearch).post(`https://webshop.fressnapf.hu/?woof_text=${searchItem}&post_type=product&swoof=1`);
+        const res = await req(appSearch).post(`/?woof_text=${searchItem}&post_type=product&swoof=1`);
         expect(res).toBe(200);
       } catch (err) {
         console.log(`${err}`);
